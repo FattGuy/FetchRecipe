a710c0e074d78a7f108403a3d3356571a72ba81c
FengCareer <career.fchang@gmail.com>
2025-02-03 03:23:52 -0600

    update the Recipe and RecipesResponse models

--- FetchRecipe/Models/Recipe.swift
+++ FetchRecipe/Models/Recipe.swift
@@ -5,3 +5,38 @@
 //  Created by Feng Chang on 2/3/25.
 //
 
+import Foundation
+
+struct Recipe: Codable, Identifiable {
+    let id: UUID
+    let name: String
+    let cuisine: String
+    let sourceUrl: URL?
+    let youtubeUrl: URL?
+    let photoUrlSmall: URL?
+    let photoUrlLarge: URL?
+    
+    enum CodingKeys: String, CodingKey {
+        case id = "uuid"
+        case name
+        case cuisine
+        case sourceUrl = "source_url"
+        case youtubeUrl = "youtube_url"
+        case photoUrlSmall = "photo_url_small"
+        case photoUrlLarge = "photo_url_large"
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        id = try container.decode(UUID.self, forKey: .id)
+        name = try container.decode(String.self, forKey: .name)
+        cuisine = try container.decode(String.self, forKey: .cuisine)
+        
+        // Gracefully handle invalid URLs
+        sourceUrl = URL(string: (try? container.decode(String.self, forKey: .sourceUrl)) ?? "")
+        youtubeUrl = URL(string: (try? container.decode(String.self, forKey: .youtubeUrl)) ?? "")
+        photoUrlSmall = URL(string: (try? container.decode(String.self, forKey: .photoUrlSmall)) ?? "")
+        photoUrlLarge = URL(string: (try? container.decode(String.self, forKey: .photoUrlLarge)) ?? "")
+    }
+}
--- FetchRecipe/Models/RecipesResponse.swift
+++ FetchRecipe/Models/RecipesResponse.swift
@@ -5,3 +5,8 @@
 //  Created by Feng Chang on 2/3/25.
 //
 
+import Foundation
+
+struct RecipeResponse: Codable {
+    let recipes: [Recipe]
+}

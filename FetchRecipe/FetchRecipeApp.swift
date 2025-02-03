e41930afe13160cf6d510810c8db46c685cc00ae
Feng Chang <fchangprivate@gmail.com>
2025-02-03 03:01:25 -0600

    Initial Commit

---
+++ FetchRecipe/FetchRecipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchRecipeApp.swift
+//  FetchRecipe
+//
+//  Created by Feng Chang on 2/3/25.
+//
+
+import SwiftUI
+
+@main
+struct FetchRecipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
